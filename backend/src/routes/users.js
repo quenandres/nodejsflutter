dd9f882305330077b37a19a8580dea53ea02a45d
Jhonatan Mejia <quenandres@gmail.com>
2020-03-21 13:59:29 -0500

    Termina backend con consulta de usuarios desde la base de datos de mongo.

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,6 +1,24 @@
 const {Router} = require('express');
 const router = Router();
+const faker = require('faker');
+
+const User = require('../models/User');
 
 
+router.get('/api/users', async (req, res) => {
+    const users = await User.find();
+    res.json({users});
+});
+
+router.get('/api/users/create', async (req, res) => {
+    for (let i = 0; i < 5; i++) {
+        await User.create({
+            firtsName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            avatar: faker.image.avatar()
+        });
+    }
+    res.json({message: '5 Usuarios creados'});
+});
 
 module.exports = router;
